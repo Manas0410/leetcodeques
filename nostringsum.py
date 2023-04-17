1b38f1988556a22136f6a50ae0cd85fac23bcc6f
Manas0410 <manasshrivastava0410@gmail.com>
2023-04-17 16:40:02 -0700

    stringadd

---
+++ nostringsum.py
@@ -0,0 +1,23 @@
+def stringSum(num1, num2):
+    i, j = len(num1)-1, len(num2)-1
+    carry = 0
+    sum = 0
+    res = ''
+    while i >= 0 or j >= 0 or carry > 0:
+        if i > 0:
+            sum += int(num1[i])
+        else:
+            sum += 0
+        if j > 0:
+            sum += int(num2[j])
+        else:
+            sum += 0
+        sum += carry
+        res += str(sum % 10)
+        carry = sum//10
+        i -= 1
+        j -= 1
+    return res[::-1]
+
+
+print(stringSum('11', '123'))
--- stringcompressII.py
+++ stringcompressII.py
@@ -1,20 +1,41 @@
-https: // leetcode.com/problems/string-compression/description/
+def compress(chars):
+    res = ''
+    dictval = {}
+
+    for i in chars:
+        if i in dictval:
+            dictval[i] += 1
+        else:
+            dictval[i] = 1
+    for i, j in dictval.items():
+        if j == 1:
+            res += i
+        else:
+            res += i + str(j)
+    chars = list(res)
+    return chars
 
 
-class Solution:
-    def compress(self, chars: List[str]) -> int:
-        res = ''
-        dictval = {}
+print(compress(["a", "a", "b", "b", "c", "c", "c"]))
 
-        for i in chars:
-            if i in dictval:
-                dictval[i] += 1
-            else:
-                dictval[i] = 1
-        for i, j in dictval.items():
-            if j == 1:
-                res += i
-            else:
-                res += i + str(j)
-        chars = list(res)
-        return len(chars)
+
+# https: // leetcode.com/problems/string-compression/description/
+
+
+# class Solution:
+#     def compress(self, chars: List[str]) -> int:
+#         res = ''
+#         dictval = {}
+
+#         for i in chars:
+#             if i in dictval:
+#                 dictval[i] += 1
+#             else:
+#                 dictval[i] = 1
+#         for i, j in dictval.items():
+#             if j == 1:
+#                 res += i
+#             else:
+#                 res += i + str(j)
+#         chars = list(res)
+#         return len(chars)
