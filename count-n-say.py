77d7718423c75fa9d0cc3d7bbc35ca4db1697d47
Manas0410 <manasshrivastava0410@gmail.com>
2023-04-19 12:03:22 -0700

    cns

--- count-n-say.py
+++ count-n-say.py
@@ -1,34 +1,36 @@
-def fragment(n):  # to get the fragments
-    sum, i = 0, 1
-    frag = ''
-    while sum <= n:
-        counter = sum
-        sum += i
-        if sum < n:
-            frag += str(i)
-            i += 1
-        elif sum > n:
-            sum = counter
-            i = 1
-        else:
-            frag += str(i)
-            break
-    return frag
+class Solution(object):
+    def countAndSay(self, n):
+        def fragment(n):  # to get the fragments
+            sum, i = 0, 1
+            frag = ''
+            while sum <= n:
+                counter = sum
+                sum += i
+                if sum < n:
+                    frag += str(i)
+                    i += 1
+                elif sum > n:
+                    sum = counter
+                    i = 1
+                else:
+                    frag += str(i)
+                    break
+            return frag[::-1]
 
+        def compress(s):
 
-def compress(s):
-
-    count = 1
-    cmprs = ''
-    for i in range(1, len(s)):
-        if s[i] == s[i-1]:
-            count += 1
-        else:
-            cmprs += str(count)+s[i-1]
             count = 1
-    cmprs += str(count)+s[-1]
-    return cmprs
-
-
-str = fragment(3)
-print(compress(str))
+            cmprs = ''
+            for i in range(1, len(s)):
+                if s[i] == s[i-1]:
+                    count += 1
+                else:
+                    cmprs += str(count)+s[i-1]
+                    count = 1
+            cmprs += str(count)+s[-1]
+            return cmprs
+        if n == 1:
+            return "1"
+        else:
+            a = fragment(n-1)
+            return compress(a)
